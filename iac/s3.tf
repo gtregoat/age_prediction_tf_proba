c6b551951e3f5b2a5159c8ece3297b86a8292f4f
gtregoat <tregoat.gabriel@gmail.com>
2022-04-04 20:46:36 +0200

    Adding s3 bucket for the dataset

--- iac/s3.tf
+++ iac/s3.tf
@@ -12,3 +12,13 @@ resource "aws_s3_bucket_acl" "pipe_bucket_acl" {
   bucket = aws_s3_bucket.pipe_bucket.id
   acl    = "private"
 }
+
+
+resource "aws_s3_bucket" "age_detection_dataset" {
+  bucket = "sagemaker-age-detection-training-dataset"
+
+  tags = {
+    age_prediction_tf_proba = "dataset"
+  }
+
+}
